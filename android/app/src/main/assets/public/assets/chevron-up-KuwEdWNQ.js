99cafb7a6b80ede31137d1085b8c82add1712eef
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-09-09 12:28:08 -0300

    chore: projeto Android — 2.assistente v2.0.0

---
+++ android/app/src/main/assets/public/assets/chevron-up-KuwEdWNQ.js
@@ -0,0 +1,1 @@
+import{p as o}from"./index-MfVsal_x.js";const e=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("chevron-up",e);export{n as C};
---
+++ dist/assets/terminal-DjCaBnXe.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-MfVsal_x.js";const o=[["path",{d:"M12 19h8",key:"baeox8"}],["path",{d:"m4 17 6-6-6-6",key:"1yngyt"}]],a=e("terminal",o);export{a as T};
